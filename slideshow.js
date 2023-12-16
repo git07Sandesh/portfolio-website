1baa95fba06e8d9c2657e432a73fc19c3401aab5
Sandesh Bhattarai <sandesh1122bhattarai@gmail.com>
2023-12-16 13:01:34 -0600

    new change

--- slideshow.js
+++ slideshow.js
@@ -1,15 +1,143 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    var scrollLinks = document.querySelectorAll('.scroll-link');
-    scrollLinks.forEach(function(scrollLink) {
-        scrollLink.addEventListener('click', function(event) {
-            event.preventDefault();
-            var targetId = this.getAttribute('href').substring(1);
-            var targetElement = document.getElementById(targetId);
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: 'smooth'
-            });
-        });
-    });
+
+/* This is for nav bar... But is not working   */
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if(navToggle){
+navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
 });
+}
+if(navClose){
+navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+});
+}
+// Remove menu mobile
+const navLink = document.querySelectorAll('.nav_link');
+function linkAction(){
+const navMenu = document.getElementById('nav-menu');
+navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+
+ /*         this is for auto typing effect */
+var typed = new Typed(".auto-type", {
+    strings: ["Student","Active Learner", "Coder"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+    });
+
+     
+/*              This is for arrows in skills        */ 
+const skillsContent = document.getElementsByClassName('skills_content'),
+skillsHeader = document.querySelectorAll('.skills_header');
+
+function toogleSkills(){
+    let itemClass = this.parentNode.className;
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close';
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach((eL) =>{
+    eL.addEventListener('click',toogleSkills);
+})
+
+/**This section is for qualification section Toggles between work and education */
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
+
+
+/*              This one is for scroll section active link              */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        let sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }
+        else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*          Dark light mode                  */
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'fa-sun'
+
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-moon' : 'fa-sun'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'fa-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+    
+})
+
+
+
+/*
+var backToTopBtn = document.getElementById("backToTopBtn");
+        window.onscroll = function() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                backToTopBtn.style.display = "block";
+            } else {
+                backToTopBtn.style.display = "none";
+            }
+        };
+        // When the user clicks on the button, scroll to the top of the document
+        backToTopBtn.addEventListener("click", function() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+        });
+*/
